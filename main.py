4a0c0be7c845296cdd0af32db156c859956f4594
JesusManuelLap <a23010170@uhipocrstes.edu.mx>
2025-12-11 11:52:47 -0600

    Se eliminaron main.py y modelo.py y se añadió el estilo css

--- main.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-from interfaz import Interfaz
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = Interfaz(root)
-    root.mainloop()
--- modelo.py
+++
@@ -1,9 +0,0 @@
-import math
-
-class ModeloPlanta:
-
-    "Funcion de logistica"
-    
-    @staticmethod
-    def logistica(P0, Pmax, r, t):
-        return Pmax / (1 + ((Pmax - P0) / P0) * math.exp(-r * t))
